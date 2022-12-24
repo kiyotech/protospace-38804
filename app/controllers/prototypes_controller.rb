fed0c4e3172852f39c83f8337241ed59e14531d3
kiyotech <kiyosawatech@gmail.com>
2022-12-24 12:13:45 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,23 @@
+class PrototypesController < ApplicationController
+  def index
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+  
+  def create
+    Prototype.create(prototype_params)
+    if current_user.create(prototype_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+end
